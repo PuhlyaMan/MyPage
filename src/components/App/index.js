00feed3654ad56a043d904b0ef130827be1a4caa
Sergey <dr.zad1994@gmail.com>
2019-07-01 17:43:34 +0300

    Переписал проект с использованием babel+webpack (без nano-react-app)

---
+++ src/components/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.css'
+
+const App = () => {
+    return (
+        <div>
+            <h1 className={style.header}>Hello world!!!</h1>
+        </div>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js", //входные данные
+  output: {
+    //выходные данные
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+
+  devServer: {
+    contentBase: path.join(__dirname, "dist"), //папка с ресурсами и index.html
+    compress: true,
+    port: 8080,
+    watchContentBase: true,
+    progress: true
+  },
+
+  module: {
+    //массив правил
+    rules: [
+      //Обработка js файлов
+      {
+        //все файлы с расширением .m и .js
+        test: /\.m?js$/,
+        //из каталогов node_modules или bower_components
+        exclude: /node_modules/,
+        //лоадер
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      //Обработка css
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader", //второй обработчик
+          {
+            loader: "css-loader", //первый обработчик
+            options: {
+              modules: true //стили применяются только к тем компонентам, в которые они импортированы
+            }
+          }
+        ]
+      },
+      //статические ресурсы
+      {
+        test: /\.(png|svg|jpg|gif|ico)$/,
+        use: ["file-loader"]
+      }
+    ]
+  }
+};
