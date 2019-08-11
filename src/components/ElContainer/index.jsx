e6248889b46423149718f065ca6f0193af4723a5
Sergey <dr.zad1994@gmail.com>
2019-08-12 00:17:24 +0300

    Add material-ui, TODO: remove react-bootstrap. Add Footer!

--- src/components/ElContainer/index.jsx
+++ src/components/ElContainer/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 // TODO: Material-ui
-import { Container } from 'react-bootstrap';
+// import { Container } from 'react-bootstrap';
+import Container from '@material-ui/core/Container';
 import AboutMe from './AboutMe';
 import Menu from './Menu';
+import Footer from './Footer';
 import CommunicationForm from './CommunicationForm';
 
 export default function ElContainer() {
   return (
-    <Container>
+    <Container maxWidth="lg">
       <AboutMe />
       <Menu />
       <CommunicationForm />
+      <Footer />
     </Container>
   );
 }
